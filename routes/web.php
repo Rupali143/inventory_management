f91718164a4f650251c1b9bf496b8f7002420b4f
Rupali Satpute <rupalisatpute289@gmail.com>
2020-01-20 15:32:34 +0530

    Changes related Authentication

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,18 @@
 |
 */
 
-Route::get('/home', function () { return view('index'); })->name('home')->middleware('auth','admin');
+Auth::routes();
+//Route::get('/home', function () { return view('index'); })->name('home')->middleware('auth','admin');
 
 //Route::post('/login',function(){return view('users/login');})->name('login');
 Route::get('/',array('as'=>'home','uses'=>'LoginController@index'));
 
 Route::post('/login',array('as'=>'login','uses' =>'LoginController@doLogin'));
 
-Route::get('/logout',array('as' => 'logout','uses' =>'LoginController@logout'));
-Auth::routes();
 
-Auth::routes(['login' => false]);
-Route::get('/home1', 'HomeController@index')->name('home1');
 
-//Route::post('/register',array('as'=>'register','uses' => 'RegisterController@save'));
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    Route::get('/logout',array('as' => 'logout','uses' =>'LoginController@logout'));
+});
